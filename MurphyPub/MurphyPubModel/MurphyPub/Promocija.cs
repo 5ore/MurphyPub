cc1fed03e0b13e1df81bfc6ce2488353f88f5f16
Luka Zdravkovic <lukazd99@outlook.com>
2018-09-04 19:26:47 +0200

    Ekrani aplikacije i klase za podatke
    Napravljeni ekrani i layout za svaki activity u android aplikaciji i pripremljene klase za rad sa bazom.

---
+++ MurphyPub/MurphyPubModel/MurphyPub/Promocija.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MurphyPub
+{
+    public class Promocija
+    {
+        private string Naziv;
+        private string Opis;
+        private string Tip;
+        private float Cena;
+        private DateTime Datum;
+        private string Slika;
+
+        public Promocija(string naziv, string opis, string tip, float cena, DateTime datum, string slika)
+        {
+            Naziv = naziv;
+            Opis = opis;
+            Tip = tip;
+            Cena = cena;
+            Datum = datum;
+            Slika = slika;
+        }
+    }
+}
